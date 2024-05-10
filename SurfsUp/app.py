54c32fb0db3dae899620f1a66918bd06d236ed85
Jaskirat Singh <jaskirat054@outlook.com>
2024-05-09 23:10:25 -0400

    adding code to flask setup

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -18,7 +18,6 @@ from flask import Flask, jsonify
 engine = create_engine("sqlite://hawaii.sqlite")
 
 # reflect an existing database into a new model
-# Base = declarative_base()
 Base = automap_base()
 # reflect the tables
 
@@ -31,7 +30,7 @@ Station= Base.classes.station
 # Create our session (link) from Python to the DB
 
 session = Session(engine)
-#################################################
+#################################################git 
 # Flask Setup
 #################################################
 
@@ -41,3 +40,81 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def home():
+    return (
+        f"Welcome to the Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt/&lt;end&gt"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    start_date = datetime.strptime(most_recent_date, '%Y-%m-%d') - timedelta(days=365)
+    precipitation_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= start_date).all()
+    session.close()
+
+    precipitation_dict = {date: prcp for date, prcp in precipitation_data}
+    return jsonify(precipitation_dict)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    station_list = session.query(Station.station).all()
+    session.close()
+
+    return jsonify(station_list)
+
+# Adding routes for /api/v1.0/tobs, /api/v1.0/<start>, and /api/v1.0/<start>/<end>
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    most_active_station = session.query(Measurement.station, func.count(Measurement.station)).\
+        group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).first()[0]
+    
+    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    start_date = datetime.strptime(most_recent_date, '%Y-%m-%d') - timedelta(days=365)
+    
+    temperature_data = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == most_active_station, Measurement.date >= start_date).all()
+    
+    session.close()
+
+    temperature_dict = {date: tobs for date, tobs in temperature_data}
+    return jsonify(temperature_dict)
+
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    session = Session(engine)
+    start_date = datetime.strptime(start, '%Y-%m-%d')
+    
+    temperature_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).all()
+    
+    session.close()
+
+    temperature_stats = {"TMIN": temperature_data[0][0], "TAVG": temperature_data[0][1], "TMAX": temperature_data[0][2]}
+    return jsonify(temperature_stats)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_date(start, end):
+    session = Session(engine)
+    start_date = datetime.strptime(start, '%Y-%m-%d')
+    end_date = datetime.strptime(end, '%Y-%m-%d')
+    
+    temperature_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date, Measurement.date <= end_date).all()
+    
+    session.close()
+
+    temperature_stats = {"TMIN": temperature_data[0][0], "TAVG": temperature_data[0][1], "TMAX": temperature_data[0][2]}
+    return jsonify(temperature_stats)
+
+if __name__ == "__main__":
+    app.run(debug=True)
