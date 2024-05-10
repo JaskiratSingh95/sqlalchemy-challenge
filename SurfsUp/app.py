aaca9f46792c6fc962fc3467a66f589256d85c6e
Jaskirat Singh <jaskirat054@outlook.com>
2024-05-09 22:58:55 -0400

    Adding changes to the jupyter notebook

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,28 +1,41 @@
 # Import the dependencies.
 
+import numpy as np
+import pandas as pd
+import datetime as dt
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.inspection import inspect
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite://hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+# Base = declarative_base()
+Base = automap_base()
 # reflect the tables
 
-
+# Base.prepare(autoload_with = engine)
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
 # Save references to each table
-
-
+Measurement = Base.classes.measurement
+Station= Base.classes.station
 # Create our session (link) from Python to the DB
 
-
+session = Session(engine)
 #################################################
 # Flask Setup
 #################################################
 
-
+app = Flask(__name__)
 
 
 #################################################
